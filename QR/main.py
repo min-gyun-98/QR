8cb5afdb2dadea75e730d28aba5b626a44b8046c
min-gyun-98 <rbsals11@naver.com>
2021-05-23 20:53:05 +0900

    Update main.py

--- QR/main.py
+++ QR/main.py
@@ -45,4 +45,6 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port, debug=True)
